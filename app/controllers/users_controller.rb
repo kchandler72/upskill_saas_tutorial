1b726291fb5b0dbee88df186165b50609dc00db2
Ken Chandler <kenatwork72@yahoo.com>
2017-08-08 13:46:38 +0000

    Added @users instance variable to index action to get all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   
+  def index
+    @users = User.all
+  end
+  
   # GET to /users/:id
   def show
    @user = User.find ( params[:id] )
-  end
-    
-  def index
-    
   end
     
 end
